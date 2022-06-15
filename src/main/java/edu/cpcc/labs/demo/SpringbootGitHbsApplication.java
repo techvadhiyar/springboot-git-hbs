c4bf6fc1e183cd9aa2717dae837c36fdd40236eb
cpccharry <harry.subramaniam@cpcc.edu>
2022-06-15 14:32:24 -0400

    Initial version.

---
+++ src/main/java/edu/cpcc/labs/demo/SpringbootGitHbsApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitHbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitHbsApplication.class, args);
+	}
+
+}
